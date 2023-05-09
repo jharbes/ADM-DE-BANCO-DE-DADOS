618a32593150c5e0066211903fb43d31abd0b42f
Jorge Harbes <jharbes@hotmail.com>
2023-05-09 16:17:29 -0300

    Create permissoes-e-usuarios.sql

---
+++ lista-exercicios-06/permissoes-e-usuarios.sql
@@ -0,0 +1,93 @@
+/*
+### Criando usuário para BOB e seu time
+*/
+-- para facilitar todas as senhas seram 12434
+
+CREATE USER 'Bob' IDENTIFIED BY '1234';
+CREATE USER 'Betelgeuse' IDENTIFIED BY '1234';
+CREATE USER 'Pitty' IDENTIFIED BY '1234';
+CREATE USER 'Kelly' IDENTIFIED BY '1234';
+
+-- acessos de BOB
+
+GRANT ALL ON pessoas.* TO 'Bob' WITH GRANT OPTION;
+GRANT SELECT ON endereco.* TO 'Bob' WITH GRANT OPTION;
+REVOKE CREATE FROM 'Bob';
+
+-- acessos de Betelgeuse
+
+GRANT SELECT ON endereco.* TO 'Betelgeuse';
+GRANT SELECT ON pessoas.* TO 'Betelgeuse';
+
+-- acessos de Pitty
+
+GRANT SELECT, INSERT ON pessoas.usuario TO 'Pitty';
+
+-- acessos de Kelly
+
+GRANT ALL ON pessoas.* TO 'Kelly';
+GRANT ALL ON endereco.* TO 'Kelly';
+GRANT ALL ON int_ensino.* TO 'Kelly';
+GRANT CREATE VIEW TO 'Kelly';
+
+/*
+### Criando usuário para Alice e seu time
+*/
+
+CREATE USER 'Alice' IDENTIFIED BY '1234';
+CREATE USER 'Thiago' IDENTIFIED BY '1234';
+CREATE USER 'Benedita' IDENTIFIED BY '1234';
+
+-- acessos de Alice
+
+GRANT SELECT ON int_ensino.* TO 'Alice' WITH GRANT OPTION;
+GRANT SELECT ON endereco.* TO 'Alice' WITH GRANT OPTION;
+REVOKE CREATE FROM 'Alice';
+
+-- acessos de Thiago
+
+GRANT SELECT ON endereco.* TO 'Thiago';
+GRANT SELECT ON pessoas.usuario TO 'Thiago';
+GRANT SELECT ON pessoas.contato TO 'Thiago';
+
+-- acessos de Benedita
+
+GRANT SELECT ON pessoas.usuario TO 'Benedita';
+GRANT SELECT ON pessoas.contato TO 'Benedita';
+
+/*
+### Criando usuário para Nina e seu time
+*/
+
+CREATE USER 'Nina' IDENTIFIED BY '1234';
+CREATE USER 'Kyara' IDENTIFIED BY '1234';
+CREATE USER 'Ganimedes' IDENTIFIED BY '1234';
+CREATE USER 'Folo' IDENTIFIED BY '1234';
+
+-- acessos de Nina
+
+GRANT CREATE TO 'Nina';
+GRANT ALL ON int_ensino.* TO 'Nina' WITH GRANT OPTION;
+GRANT ALL ON pessoas.* TO 'Nina';
+GRANT SELECT ON pessoas.* TO 'Nina' WITH GRANT OPTION;
+
+-- acessos de Kyara
+
+GRANT ALL ON int_ensino.* TO 'Kyara' WITH GRANT OPTION;
+GRANT ALL ON pessoas.* TO 'Kyara' WITH GRANT OPTION;
+GRANT ALL ON endereco.* TO 'Kyara' WITH GRANT OPTION;
+
+-- acessos de Ganimedes
+
+GRANT SELECT, INSERT, ALTER ON int_ensino.* TO 'Ganimedes';
+GRANT SELECT, INSERT, ALTER ON pessoas.pessoas TO 'Ganimedes';
+
+-- acessos de Foló
+
+GRANT SELECT ON pessoas.pessoas TO 'Folo';
+GRANT SELECT ON int_ensino.curso TO 'Folo';
+
+
+
+SELECT *
+FROM mysql.user;
