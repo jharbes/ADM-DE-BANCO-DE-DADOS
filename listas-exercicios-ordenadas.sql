10fd6ba094d6a742128df7b87850c640b2b3591c
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-05-10 09:39:57 -0300

    Create listas-exercicios-ordenadas.sql

---
+++ listas-exercicios-ordenadas.sql
@@ -0,0 +1,14 @@
+/*
+
+Só pra facilitar 
+
+1 - Instalando o MariaDB e o PHP MyAdmin com Docker
+2 - Instalando o SQL Server e o Azure Data Studio com Docker
+3 - Backup e Restore no MySQL/MariaDB
+4 - Criação de View
+5 - Views e Stored Procedures
+6 - Usuários e Permissões
+7 - Banco de Dados em Serviços Distintos
+8 - Projeto MVC com Banco Relacional e MongoDB
+
+*/
