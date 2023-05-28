6566f137bca0073b41379bb8cf30de3bf8f8d807
Jorge Harbes <jharbes@hotmail.com>
2023-05-28 15:45:45 -0300

    Update permissoes-e-usuarios.sql

--- lista-exercicios-06/permissoes-e-usuarios.sql
+++ lista-exercicios-06/permissoes-e-usuarios.sql
@@ -1,5 +1,5 @@
 /*
-### Criando usuário para BOB e seu time
+### Criando todos os usuarios
 */
 -- senha default 123456
 
@@ -7,6 +7,19 @@ CREATE USER 'Bob' IDENTIFIED BY '123456';
 CREATE USER 'Betelgeuse' IDENTIFIED BY '123456';
 CREATE USER 'Pitty' IDENTIFIED BY '123456';
 CREATE USER 'Kelly' IDENTIFIED BY '123456';
+CREATE USER 'Alice' IDENTIFIED BY '123456';
+CREATE USER 'Thiago' IDENTIFIED BY '123456';
+CREATE USER 'Benedita' IDENTIFIED BY '123456';
+CREATE USER 'Nina' IDENTIFIED BY '123456';
+CREATE USER 'Kyara' IDENTIFIED BY '123456';
+CREATE USER 'Ganimedes' IDENTIFIED BY '123456';
+CREATE USER 'Folo' IDENTIFIED BY '123456';
+
+
+-- listando usuarios existentes
+SELECT User, Host FROM mysql.user;
+
+
 
 -- acessos de BOB
 
@@ -14,35 +27,46 @@ REVOKE CREATE ON *.* FROM 'Bob';
 GRANT ALL ON pessoas.* TO 'Bob' WITH GRANT OPTION;
 GRANT SELECT ON endereco.* TO 'Bob' WITH GRANT OPTION;
 
+
+
+-- acessos de Alice
+
+REVOKE CREATE ON *.* FROM 'Alice';
+GRANT SELECT ON int_ensino.* TO 'Alice' WITH GRANT OPTION;
+GRANT SELECT ON endereco.* TO 'Alice' WITH GRANT OPTION;
+
+
+
+-- acessos de NINA
+
+GRANT CREATE ON *.* TO 'Nina';
+GRANT ALL ON int_ensino.* TO 'Nina' WITH GRANT OPTION;
+GRANT ALL ON pessoas.* TO 'Nina';
+GRANT SELECT ON pessoas.* TO 'Nina' WITH GRANT OPTION;
+
+
+
 -- acessos de Betelgeuse
 
 GRANT SELECT ON endereco.* TO 'Betelgeuse';
 GRANT SELECT ON pessoas.* TO 'Betelgeuse';
 
+
+
 -- acessos de Pitty
 
 GRANT SELECT, INSERT ON pessoas.usuario TO 'Pitty';
+
+
 
 -- acessos de Kelly
 
 GRANT ALL ON pessoas.* TO 'Kelly';
 GRANT ALL ON endereco.* TO 'Kelly';
 GRANT ALL ON int_ensino.* TO 'Kelly';
-GRANT CREATE VIEW TO 'Kelly';
+GRANT CREATE VIEW ON *.* TO 'Kelly';
 
-/*
-### Criando usuário para Alice e seu time
-*/
 
-CREATE USER 'Alice' IDENTIFIED BY '123456';
-CREATE USER 'Thiago' IDENTIFIED BY '123456';
-CREATE USER 'Benedita' IDENTIFIED BY '123456';
-
--- acessos de Alice
-
-GRANT SELECT ON int_ensino.* TO 'Alice' WITH GRANT OPTION;
-GRANT SELECT ON endereco.* TO 'Alice' WITH GRANT OPTION;
-REVOKE CREATE FROM 'Alice';
 
 -- acessos de Thiago
 
@@ -50,26 +74,16 @@ GRANT SELECT ON endereco.* TO 'Thiago';
 GRANT SELECT ON pessoas.usuario TO 'Thiago';
 GRANT SELECT ON pessoas.contato TO 'Thiago';
 
+
+
+
 -- acessos de Benedita
 
 GRANT SELECT ON pessoas.usuario TO 'Benedita';
 GRANT SELECT ON pessoas.contato TO 'Benedita';
 
-/*
-### Criando usuário para Nina e seu time
-*/
 
-CREATE USER 'Nina' IDENTIFIED BY '123456';
-CREATE USER 'Kyara' IDENTIFIED BY '123456';
-CREATE USER 'Ganimedes' IDENTIFIED BY '123456';
-CREATE USER 'Folo' IDENTIFIED BY '123456';
 
--- acessos de Nina
-
-GRANT CREATE TO 'Nina';
-GRANT ALL ON int_ensino.* TO 'Nina' WITH GRANT OPTION;
-GRANT ALL ON pessoas.* TO 'Nina';
-GRANT SELECT ON pessoas.* TO 'Nina' WITH GRANT OPTION;
 
 -- acessos de Kyara
 
@@ -77,10 +91,15 @@ GRANT ALL ON int_ensino.* TO 'Kyara' WITH GRANT OPTION;
 GRANT ALL ON pessoas.* TO 'Kyara' WITH GRANT OPTION;
 GRANT ALL ON endereco.* TO 'Kyara' WITH GRANT OPTION;
 
+
+
 -- acessos de Ganimedes
 
 GRANT SELECT, INSERT, ALTER ON int_ensino.* TO 'Ganimedes';
 GRANT SELECT, INSERT, ALTER ON pessoas.pessoas TO 'Ganimedes';
+
+
+
 
 -- acessos de Foló
 
